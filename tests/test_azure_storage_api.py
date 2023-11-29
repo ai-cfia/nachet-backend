18690353208158f9f7cc746f383b899762a1ba88
reda <tarzaltreda@gmail.com>
2023-11-29 14:50:48 -0500

    issue #16: strip whitelines

--- tests/test_azure_storage_api.py
+++ tests/test_azure_storage_api.py
@@ -14,32 +14,31 @@ import asyncio
 class TestMountContainerFunction(unittest.TestCase):
     @patch("azure.storage.blob.BlobServiceClient.from_connection_string")
     def test_mount_existing_container(self, MockFromConnectionString):
-    
+
         # mock the client container
         mock_container_client = Mock()
         mock_container_client.exists.return_value = True
-    
+
         # Mock the method that returns the blob_list
         mock_blobs = [Mock(), Mock(), Mock()]
         mock_container_client.list_blobs.return_value = iter(mock_blobs)
-    
+
         # mock the blob service client
         mock_blob_service_client = MockFromConnectionString.return_value
         mock_blob_service_client.get_container_client.return_value = (
             mock_container_client
         )
-    
         connection_string = "test_connection_string"
         container_name = "testcontainer"
-    
+
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         result = loop.run_until_complete(
             mount_container(connection_string, container_name)
         )
-    
+
         print(result == mock_container_client)
-    
+
         self.assertEqual(result, mock_container_client)
 
 
@@ -52,32 +51,32 @@ class TestMountContainerFunction(unittest.TestCase):
         # mock the client container and blob service client
         mock_container_client = Mock()
         mock_container_client.exists.return_value = False
-    
+
         # Mock the method that returns the blob_list
         mock_blobs = [Mock(), Mock(), Mock()]
         mock_container_client.list_blobs.return_value = mock_blobs  # Return the list directly
-    
+
         mock_blob_service_client = MockFromConnectionString.return_value
         mock_blob_service_client.get_container_client.return_value = (
             mock_container_client
         )
-    
+
         # Simulate that a new container is created
         mock_new_container_client = Mock()
         mock_blob_service_client.create_container.return_value = (
             mock_new_container_client
         )
-    
+
         connection_string = "test_connection_string"
         container_name = "testcontainer"
         expected_container_name = "user-{}".format(container_name)
-    
+
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         result = loop.run_until_complete(
             mount_container(connection_string, container_name, create_container=True)
         )
-    
+
         mock_blob_service_client.create_container.assert_called_once_with(
             expected_container_name
         )
