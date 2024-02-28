3cddb43b8554a76e7c15c7af7c00c1326617b8fc
Maxence Guindon <maxence.guindon@inspection.gc.ca>
2024-02-28 15:31:36 +0000

    fixes #51: Add test get pipeline info unsuccessful

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,4 @@
-from tests.test_azure_storage_api import TestMountContainerFunction, TestGetBlob
+from tests.test_azure_storage_api import TestMountContainerFunction, TestGetBlob, testGetPipeline
 
 a = TestMountContainerFunction()
 a.test_mount_existing_container()
@@ -8,3 +8,7 @@ a.test_mount_nonexisting_container_no_create()
 b = TestGetBlob()
 b.test_get_blob_successful()
 b.test_get_blob_unsuccessful()
+
+c = testGetPipeline()
+c.test_get_pipeline_info_unsuccessful()
+c.test_get_pipeline_info_successful()
