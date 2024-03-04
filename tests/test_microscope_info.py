bd820addb2dc04169f60f6592cbe0d76b52cd554
Maxence Guindon <maxence.guindon@inspection.gc.ca>
2024-03-04 14:25:52 +0000

    fixes #29: Add element to collect exiftags
    commit this change to not lose them to codespaces repo ending

--- tests/test_microscope_info.py
+++ tests/test_microscope_info.py
@@ -1,6 +1,6 @@
 import microscope.microscope_info as m
 
-path=r"/workspaces/nachet-backend/docs/asssets/image/WIN_20240131_10_23_04_Pro.jpg"
+path=r"/workspaces/nachet-backend/docs/asssets/image/test_02.jpg"
 
 def test_print_exif_info():
     data = m.get_picture_details(path)
