25f6d6bba1b5794f3a97ac3207b4504dbc18434a
ChromaticPanic <joseffus.santos@inspection.gc.ca>
2024-12-04 23:19:45 -0500

    143 deploy swin 22 spp model

--- model/__init__.py
+++ model/__init__.py
@@ -4,6 +4,7 @@ from model.test import request_inference_from_test
 from model.six_seeds import request_inference_from_nachet_6seeds
 
 request_function = {
+    "swin-22-spp": request_inference_from_swin,
     "swinv1-base-dataaugv2-1": request_inference_from_swin,
     "seed-detector-1": request_inference_from_seed_detector,
     "swinv1-base-dataaugv2-2": request_inference_from_swin,
