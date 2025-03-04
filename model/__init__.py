4749831e6763350c78eb68d428606c47d0e52bfd
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-03-04 00:41:54 +0000

    Add ensemble inference functions for SWIN model and update request mapping

--- model/__init__.py
+++ model/__init__.py
@@ -3,15 +3,16 @@ from model.seed_detector import request_inference_from_seed_detector
 from model.test import request_inference_from_test
 from model.six_seeds import request_inference_from_nachet_6seeds
 from model.torch_swin import request_inference_from_torch_swin
+from model.torch_ensemble import request_inference_ensemble_a, request_inference_ensemble_b
 
 request_function = {
     "local-seed-detector-rcnn": request_inference_from_seed_detector,
     "local-swin-15-spp": request_inference_from_swin,
     "local-swin-22-spp": request_inference_from_swin,
-    "local-swin-27-spp": request_inference_from_torch_swin,
-    "local-swin-15e-spp": request_inference_from_torch_swin,
-    "swin-27-spp": request_inference_from_torch_swin,
-    "swin-15e-spp": request_inference_from_torch_swin,
+    "local-swin-27-spp": request_inference_ensemble_a,
+    "local-swin-15e-spp": request_inference_ensemble_b,
+    "swin-27-spp": request_inference_ensemble_a,
+    "swin-15e-spp": request_inference_ensemble_b,
     "swin-22-spp": request_inference_from_swin,
     "swinv1-base-dataaugv2-1": request_inference_from_swin,
     "seed-detector-1": request_inference_from_seed_detector,
---
+++ model/torch_ensemble.py
@@ -0,0 +1,114 @@
+"""
+This file contains the function that requests the inference and processes the data from
+the swin model.
+"""
+
+import json
+from collections import namedtuple
+from urllib.error import URLError
+from urllib.request import Request, urlopen
+from model.model_exceptions import ModelAPIError
+
+
+class SwinModelAPIError(ModelAPIError):
+    pass
+
+
+SPECIES_LIST = [
+    "012 Ambrosia artemisiifolia",
+    "013 Ambrosia trifida",
+    "014 Ambrosia psilostachya",
+]
+
+
+def process_swin_result(img_box: dict, results: dict) -> list:
+    """
+    Args:
+        img_box (dict): The image box containing the bounding boxes and labels.
+        results (dict): The results from the model containing the detected seeds.
+
+    Returns:
+        list: The updated image box with modified labels and scores.
+    """
+    for i, result in enumerate(results):
+        img_box[0]["boxes"][i]["label"] = result[0].get("label")
+        img_box[0]["boxes"][i]["score"] = result[0].get("score")
+        img_box[0]["boxes"][i]["topN"] = [d for d in result]
+
+    # Adding the "filename" field (mandatory)
+    img_box[0]["filename"] = "default_filename"
+
+    return img_box
+
+
+async def request_inference_ensemble_a(model: namedtuple, previous_result: "dict"):
+    """
+    Perform inference using the SWIN model on a list of images.
+
+    Args:
+        model (namedtuple): The SWIN model to use for inference.
+        previous_result (dict): The previous result containing the images to perform inference on.
+
+    Returns:
+        The result of the inference.
+
+    Raises:
+        ProcessInferenceResultsError: If an error occurs while processing the request.
+    """
+    try:
+        results = []
+        for img in previous_result.get("images"):
+            headers = {
+                "Content-Type": model.content_type,
+                "Authorization": ("Bearer " + model.api_key),
+                model.deployment_platform: model.name,
+            }
+            body = json.dumps({"image_data": img}).encode("utf-8")
+            req = Request(model.endpoint, body, headers, method="POST")
+            response = urlopen(req)
+            result = response.read()
+            result_json = json.loads(result.decode("utf8"))
+            results.append(result_json)
+
+        print(json.dumps(results, indent=4))  # TODO Transform into logging
+
+        # return process_swin_result(previous_result.get("result_json"), results)
+        return {
+            "result_json": process_swin_result(
+                previous_result.get("result_json"), results
+            ),
+            "images": previous_result.get("images"),
+        }
+    except (
+        TypeError,
+        IndexError,
+        AttributeError,
+        URLError,
+        json.JSONDecodeError,
+    ) as error:
+        print(error)
+        raise SwinModelAPIError(
+            f"An error occurred while processing the request:\n {str(error)}"
+        ) from error
+
+
+async def request_inference_ensemble_b(model: namedtuple, previous_result: "dict"):
+    """
+    Perform inference on images that are in the specified species list.
+    """
+    try:
+        results = []
+        # get indexes of images that are in the species list
+        print("previous_result: ", previous_result)
+        return 0
+    except (
+        TypeError,
+        IndexError,
+        AttributeError,
+        URLError,
+        json.JSONDecodeError,
+    ) as error:
+        print(error)
+        raise SwinModelAPIError(
+            f"An error occurred while processing the request:\n {str(error)}"
+        ) from error
